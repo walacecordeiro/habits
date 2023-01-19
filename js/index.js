2f8a1dde9326542eeda9c337184456abc96e3293
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-01-19 20:44:16 -0300

    update add days function; addition of a new function to save cached data

--- js/index.js
+++ js/index.js
@@ -1,10 +1,29 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
 
-const data = {
-    run: ['01-01', '01-02', '01-03', '01-04', '01-05', '01-06',],
-    training: ['01-01']
+const registerBtn = document.querySelector('header button')
+
+registerBtn.addEventListener('click', add)
+form.addEventListener('change', save)
+
+
+function add(){
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists){
+        alert(`🔴 Dia de hoje ${today} já está incluso 🔴`)
+        return
+    }
+    
+    alert(`✔ Dia de hoje ${today} adicionado com sucesso ✔`)
+    nlwSetup.addDay(today)
 }
 
+function save(){
+    localStorage.setItem('dayHabits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('dayHabits')) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
